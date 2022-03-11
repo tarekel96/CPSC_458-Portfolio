b4a4189de22fee4996e0353e22aa78f52723f782
Tarek <elhaj102@mail.chapman.edu>
2022-03-10 18:39:00 -0800

    committing progress

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var particles = Particles.init({
+	selector: '.background',
+	color: [ '#64ffda', '#404B69', '#DBEDF3' ],
+	connectParticles: true
+});
+// function pause() {
+// 	console.log(window.pJSDom);
+// 	this._tempFn = window.pJSDom[0].pJS.fn.vendors.draw;
+// 	window.pJSDom[0].pJS.fn.vendors.draw = () => {};
+// }
+// function play() {
+// 	window.pJSDom[0].pJS.fn.vendors.draw = this._tempFn;
+// 	window.pJSDom[0].pJS.fn.vendors.draw();
+// }
+// pause();
