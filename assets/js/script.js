3c196681669a5b8eb7e83f6d74e47e3a6caf9bf2
Tarek <elhaj102@mail.chapman.edu>
2022-03-18 14:45:48 -0700

    made the hero functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,36 @@ window.onload = () => {
 	const homeLink = document.querySelector('#home-link');
 	const bioLink = document.querySelector('#bio-link');
 	const projectsLink = document.querySelector('#projects-link');
+	const leftArrow = document.querySelector('#leftArrow');
+	const rightArrow = document.querySelector('#rightArrow');
+	const cards = document.querySelectorAll('.project-card');
+	let slideCounter = 0;
 
+	// sets all of slides to display none
+	const reset = () => {
+		for (let i = 0; i < cards.length; i++) {
+			// cards[i].style.display = 'none !important';
+			cards[i].classList.add('none');
+		}
+	};
+	const slideRight = () => {
+		reset();
+
+		cards[slideCounter + 1].classList.remove('none');
+		slideCounter++;
+	};
+	// sets the previous slide to block to display that image
+	function slideLeft() {
+		reset();
+		cards[slideCounter - 1].classList.remove('none');
+		slideCounter--;
+	}
+	// reset();
+	// initial the slides
+	function startSlide() {
+		reset();
+		cards[0].classList.remove('none');
+	}
 	// Default State
 	introSection.style.display = 'block';
 	bioSection.style.display = 'none';
@@ -26,5 +55,26 @@ window.onload = () => {
 	// Event Listeners
 	homeLink.addEventListener('click', handleSwitch(introSection, [ bioSection, projectsSection ]));
 	bioLink.addEventListener('click', () => handleSwitch(bioSection, [ introSection, projectsSection ]));
-	projectsLink.addEventListener('click', () => handleSwitch(projectsSection, [ introSection, bioSection ]), 'flex');
+	projectsLink.addEventListener(
+		'click',
+		() => {
+			handleSwitch(projectsSection, [ introSection, bioSection ]);
+			startSlide();
+		},
+		'flex'
+	);
+
+	rightArrow.addEventListener('click', () => {
+		if (slideCounter === cards.length - 1) {
+			slideCounter = -1;
+		}
+		slideRight();
+	});
+
+	leftArrow.addEventListener('click', () => {
+		if (slideCounter === 0) {
+			slideCounter = cards.length;
+		}
+		slideLeft();
+	});
 };
