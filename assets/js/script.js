e72168879d82359e709cd820562dda523c5d1649
Tarek <elhaj102@mail.chapman.edu>
2022-03-18 14:13:30 -0700

    committing progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,23 +8,23 @@ window.onload = () => {
 	const mainSection = document.querySelector('main');
 	const introSection = document.querySelector('#intro-section');
 	const bioSection = document.querySelector('#bio-section');
-	const resumeSection = document.querySelector('#resume-section');
+	const projectsSection = document.querySelector('#projects-section');
 	const homeLink = document.querySelector('#home-link');
 	const bioLink = document.querySelector('#bio-link');
-	const resumeLink = document.querySelector('#resume-link');
+	const projectsLink = document.querySelector('#projects-link');
 
 	// Default State
 	introSection.style.display = 'block';
 	bioSection.style.display = 'none';
-	resumeSection.style.display = 'none';
+	projectsSection.style.display = 'none';
 	// Helper
-	const handleSwitch = (section, otherSections) => {
+	const handleSwitch = (section, otherSections, display = 'block') => {
 		otherSections.forEach((item) => (item.style.display = 'none'));
-		section.style.display = 'block';
+		section.style.display = display;
 	};
 
 	// Event Listeners
-	homeLink.addEventListener('click', handleSwitch(introSection, [ bioSection, resumeSection ]));
-	bioLink.addEventListener('click', () => handleSwitch(bioSection, [ introSection, resumeSection ]));
-	resumeLink.addEventListener('click', () => handleSwitch(resumeSection, [ introSection, bioSection ]));
+	homeLink.addEventListener('click', handleSwitch(introSection, [ bioSection, projectsSection ]));
+	bioLink.addEventListener('click', () => handleSwitch(bioSection, [ introSection, projectsSection ]));
+	projectsLink.addEventListener('click', () => handleSwitch(projectsSection, [ introSection, bioSection ]), 'flex');
 };
