5ee908a71d1e4849abef30aa362679cf222e3435
Tarek <elhaj102@mail.chapman.edu>
2022-03-17 20:14:07 -0700

    committing progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,30 @@
-var particles = Particles.init({
-	selector: '.background',
-	color: [ '#64ffda', '#404B69', '#DBEDF3' ],
-	connectParticles: true
-});
-// function pause() {
-// 	console.log(window.pJSDom);
-// 	this._tempFn = window.pJSDom[0].pJS.fn.vendors.draw;
-// 	window.pJSDom[0].pJS.fn.vendors.draw = () => {};
-// }
-// function play() {
-// 	window.pJSDom[0].pJS.fn.vendors.draw = this._tempFn;
-// 	window.pJSDom[0].pJS.fn.vendors.draw();
-// }
-// pause();
+window.onload = () => {
+	// Variables
+	const particles = Particles.init({
+		selector: '.background',
+		color: [ '#64ffda', '#404B69', '#DBEDF3' ],
+		connectParticles: true
+	});
+	const mainSection = document.querySelector('main');
+	const introSection = document.querySelector('#intro-section');
+	const bioSection = document.querySelector('#bio-section');
+	const resumeSection = document.querySelector('#resume-section');
+	const homeLink = document.querySelector('#home-link');
+	const bioLink = document.querySelector('#bio-link');
+	const resumeLink = document.querySelector('#resume-link');
+
+	// Default State
+	bioSection.style.display = 'none';
+	resumeSection.style.display = 'none';
+	introSection.style.display = 'none';
+	// Helper
+	const handleSwitch = (section, otherSections) => {
+		otherSections.forEach((item) => (item.style.display = 'none'));
+		section.style.display = 'block';
+	};
+
+	// Event Listeners
+	homeLink.addEventListener('click', handleSwitch(introSection, [ bioSection, resumeSection ]));
+	bioLink.addEventListener('click', () => handleSwitch(bioSection, [ introSection, resumeSection ]));
+	resumeLink.addEventListener('click', () => handleSwitch(resumeSection, [ introSection, bioSection ]));
+};
